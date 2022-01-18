de90d450950bbf65cf671f082700b89acc66b2e1
Albert <albert.mantaras@gmail.com>
2022-01-18 20:55:22 +0100

    Start split

---
+++ Libft/ft_split.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isalpha.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 18:49:59 by amantara          #+#    #+#             */
+/*   Updated: 2022/01/16 10:59:16 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t foundNumberString(char const *s, char c){
+
+	size_t count;
+	size_t i;
+
+	count = 1;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c){
+			count++;
+		}
+	}
+}
+
+
+char **ft_split(char const *s, char c)
+{
+	
+
+	ft_strchr("encuentra la primera occurencia")
+}
+
+

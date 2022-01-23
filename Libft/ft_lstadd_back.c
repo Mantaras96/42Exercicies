3ca72c6cf251d435149f5ec26e4ca34cdef92c9c
Albert <albert.mantaras@gmail.com>
2022-01-23 21:09:41 +0100

    Subir funciones bonus

---
+++ Libft/ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isalpha.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 18:49:59 by amantara          #+#    #+#             */
+/*   Updated: 2022/01/16 10:58:10 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*backup;
+
+	backup = ft_lstlast(*lst);
+	if(!backup){
+		*lst = new;
+	}
+	else {
+		backup->next = new;
+	}
+}
---
+++ Libft/ft_lstlast.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isalpha.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amantara <amantara@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 18:49:59 by amantara          #+#    #+#             */
+/*   Updated: 2022/01/16 10:58:10 by amantara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+	t_list	*backup;
+
+	backup = NULL;
+	if (!lst) {
+		return (backup);
+	}
+
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}

e07fa4787a1af94e4d42d7f988bdc6a72d2495eb
Albert <albert.mantaras@gmail.com>
2022-01-25 21:25:14 +0100

    Arreglos

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,36 +6,30 @@
 /*   By: amantara <amantara@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 18:49:59 by amantara          #+#    #+#             */
-/*   Updated: 2022/01/16 11:07:06 by amantara         ###   ########.fr       */
+/*   Updated: 2022/01/16 10:58:10 by amantara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	size_t	i;
-	size_t	j;
-	size_t	l_dest;
-	size_t	l_src;
+	t_list *nlist;
+	t_list *p;
 
-	i = ft_strlen(dst);
-	j = 0;
-	l_dest = ft_strlen(dst);
-	l_src = ft_strlen(src);
-	if (dstsize == 0)
-		return (l_src);
-	if (l_dest > dstsize)
-		return (l_src + dstsize);
-	while (src[j] && i < dstsize - 1)
+	if (!f)
+		return (NULL);
+	nlist = NULL;
+	while (lst)
 	{
-		dst[i] = src[j];
-		i++;
-		j++;
+		p = ft_lstnew(f(lst->content));
+		if (!p)
+		{
+			ft_lstclear(&nlist, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&nlist, p);
+		lst = lst->next;
 	}
-	dst[i] = '\0';
-	if (dstsize < l_dest)
-		return (l_src + dstsize);
-	else
-		return (l_dest + l_src);
+	return (nlist);
 }

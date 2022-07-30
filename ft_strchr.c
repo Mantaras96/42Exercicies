7444a330319090c1f021ec2d324686a0ddf7020f
Albert Mantaras <34039293+Mantaras96@users.noreply.github.com>
2022-07-30 17:22:12 +0200

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -34,3 +34,23 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return (str);
 }
+
+int	ft_strchr_i(const char *s, int c)
+{
+	unsigned char	c_unsigned;
+	int				i;
+
+	i = 0;
+	if (!s)
+		return (-1);
+	c_unsigned = (unsigned char)c;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c_unsigned)
+			return (i);
+		i++;
+	}
+	if (c_unsigned == '\0')
+		return (i);
+	return (-1);
+}
